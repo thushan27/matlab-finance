21d0d5189f9b2030d5354d770478376296429af0
thushan27 <57074182+thushan27@users.noreply.github.com>
2019-10-28 14:54:11 +0000

    Update quadratic.m

--- quadratic.m
+++ quadratic.m
@@ -1,4 +1,5 @@
 % Finds the roots of a quadratic equation, where a is the coefficient of x^2, b is the coefficient of x and c is a constant
+
 function[positive, negative] = Quadratic(a,b,c)
 positive = (-b+sqrt(b^2 - 4*a*c))/2*a;
 negative = (-b-sqrt(b^2 - 4*a*c))/2*a;
