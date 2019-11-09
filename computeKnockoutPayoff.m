0a48f72fb3c73352649bc49bb47091d7b0554e6d
thushan27 <57074182+thushan27@users.noreply.github.com>
2019-11-09 11:04:03 +0000

    Update computeKnockoutPayoff.m

--- computeKnockoutPayoff.m
+++ computeKnockoutPayoff.m
@@ -1,3 +1,4 @@
+% Code provided by lecture notes. Important function used to help create compute Knockin/Asian payoffs.
 function [ payoff ] = computeKnockoutPayoff( ...
  strike, barrier, priceHistory )
 knockedOut = max( priceHistory>barrier, [], 2);
