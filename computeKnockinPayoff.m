be6bfcb9166e4f151aecaa25a7efb99c3e2f71cd
thushan27 <57074182+thushan27@users.noreply.github.com>
2020-01-02 21:17:33 +0000

    Update computeKnockinPayoff.m

--- computeKnockinPayoff.m
+++ computeKnockinPayoff.m
@@ -1,3 +1,7 @@
+% A knockin option with strike K and barrier B and maturity T is an option that pays 0 
+% if the stock price is ever below B at the end of the business day. If it reaches time T without
+% falling below the barrier, its payoff is given by max(S_T − K, 0), where S_T is the stock price at time T.
+
 function [ payoff ] = computeKnockinPayoff( ...
  strike, barrier, priceHistory )
 knockedIn = max( priceHistory>barrier, [], 2);
