b6da9308c5e822d78bce15eecbf09f1f20e455b2
thushan27 <57074182+thushan27@users.noreply.github.com>
2020-01-02 21:22:44 +0000

    Update generateBSPaths.m

--- generateBSPaths.m
+++ generateBSPaths.m
@@ -1,4 +1,7 @@
 % Code provided by lecture notes. Important function used throughout the course for example, pricing Knock-in/Asian options. 
+% Function returns a table of stock prices simulated nPaths times (rows) and over T/nSteps intervals (columns). 
+% T is the time interval, S0 the stock prices at time 0, drift mu, volatility sigma.
+
 function [ S, times ] = generateBSPaths(T,S0,mu,sigma,nPaths,nSteps)
 dt = T/nSteps;
 logS0 = log( S0);
