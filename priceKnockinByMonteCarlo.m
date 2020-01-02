550c6ff736d881cddb123b40c85d312e9fdc3576
thushan27 <57074182+thushan27@users.noreply.github.com>
2020-01-02 21:33:10 +0000

    Update priceKnockinByMonteCarlo.m

--- priceKnockinByMonteCarlo.m
+++ priceKnockinByMonteCarlo.m
@@ -1,3 +1,9 @@
+% We wish to price a knockin call option with barrier B, strike K and maturity T where one tests to see 
+% if the option has hit the barrier at nSteps evenly spaced times over the lifetime of the option.
+% The stock price follows the Black Scholes model with parameters S0, µ, r, σ as usual. We wish to write a function 
+% to price the option using Monte Carlo simulations with nPaths paths i.e generateBSPaths function. 
+% The function should also return an estimate of the error.
+
 function [price, errorEstimate]=priceKnockinByMonteCarlo(...
  strike, barrier, T,...
  S0, r, sigma, ...
