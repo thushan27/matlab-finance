c1c380787f5014e2a029d54bd4554bddc6e203d4
thushan27 <57074182+thushan27@users.noreply.github.com>
2020-01-02 21:09:41 +0000

    Update computeAsianPayoff.m

--- computeAsianPayoff.m
+++ computeAsianPayoff.m
@@ -1,3 +1,6 @@
+% An Asian call option with maturity T has its payoff determined by the average price S at the close of the last 
+% n days of trading up to and including maturity. The payoff is given by max(S − K, 0)
+
 function [ payoff ] = computeAsianPayoff( ...
  strike, priceHistory,n )
 
