b81737ed62a496287d5d11040bf7ce06aec95d71
thushan27 <57074182+thushan27@users.noreply.github.com>
2020-01-02 20:57:22 +0000

    Update and rename answerProblem3b.m to integrateAQuadratic.m

--- integrateAQuadratic.m
+++ integrateAQuadratic.m
@@ -1,3 +1,5 @@
+% Integrate the quadratic function x^2 - 2x + 2 between [1,3] using the rectangle rule (integrateNumerically).
+
 function result = answerProblem3b()
 function r = integrand ( x )
 r = x.^2 - 2.*x + 2;
